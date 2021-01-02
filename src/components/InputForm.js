87c01ac976bcf892ca60e13135a618ffcfc7db8d
Kazeem Kadiri <kazeem.kadiri@gmail.com>
2021-01-02 09:08:06 +0100

    Added code to validate user input

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -1,8 +1,15 @@
 import React from "react";
-import {Button} from "react-bootstrap";
 
-const InputForm = ({ letter,endGame }) => {
+const InputForm = ({ letter }) => {
   const inputTitles = ["name", "animal", "place", "thing"];
+
+  const validateFirstLetter = (e) => {
+    const userInput = e.target.value;
+    if (userInput.charAt(0).toUpperCase() !== letter) {
+      alert(`Word must begin with ${letter}`);
+      e.target.value = "";
+    }
+  };
 
   return (
     <>
@@ -12,7 +19,8 @@ const InputForm = ({ letter,endGame }) => {
             type="text"
             name={`${letter}-${inputTitle}`}
             id={`${letter}-${inputTitle}`}
-	    disabled
+            onChange={(e) => validateFirstLetter(e)}
+            disabled
           />
         </td>
       ))}
